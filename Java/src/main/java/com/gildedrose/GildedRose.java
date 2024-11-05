6685ae2d2b231446a19756eef89e4ce62dfc2f04
Kira Buzova <kbuzova06@gmail.cpm>
2024-11-05 20:30:13 +0200

    Extracting method, calling it updateExpiration, which performs only one function - decreases the sellIn value for items other than the "Sulfuras, Hand of Ragnaros" item. Extracting method, calling it processExpired, which handles items with sellIn < 0 term. This made it possible to clearly separate the logic of working with expired goods from the main logic of updating. Added isExpired method to check if the item is expired to reduce code duplication and make it more readable.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,27 +15,37 @@ class GildedRose {
 
     private static void updateItem(Item item) {
         updateQuality(item);
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
+        updateExpiration(item);
+        if (isExpired(item)) {
+            processExpired(item);
         }
+    }
 
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+    private static boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private static void processExpired(Item item) {
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
-                } else {
-                    item.quality = 0;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                item.quality = 0;
             }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private static void updateExpiration(Item item) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
         }
     }
 
