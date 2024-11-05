b9b5e665a33686817a5142753614b5431cc402a0
Kira Buzova <kbuzova06@gmail.cpm>
2024-11-05 23:08:10 +0200

    Lining method so that the logic for the three item types (Aged Brie, Backstage passes, and Sulfuras) has been separated, making the method more readable. The method now clearly breaks down these conditions: Aged Brie is checked first, then Backstage passes, and the last else handles all other cases.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,6 +21,45 @@ class GildedRose {
         }
     }
 
+    private static void updateQuality(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+        else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+        else {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        }
+    }
+
+    private static void updateExpiration(Item item) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+    
     private static boolean isExpired(Item item) {
         return item.sellIn < 0;
     }
@@ -39,41 +78,6 @@ class GildedRose {
         } else {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
-            }
-        }
-    }
-
-    private static void updateExpiration(Item item) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-    }
-
-    private static void updateQuality(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
             }
         }
     }
