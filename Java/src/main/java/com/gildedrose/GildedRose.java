e7e9be917336cd99a565fff0f71980cf62a17cf2
Kira Buzova <kbuzova06@gmail.cpm>
2024-11-05 23:22:44 +0200

    Simplifying the condition for "Sulfuras, Hand of Ragnaros". Previously, the code checked for this item in various conditions to avoid quality reduction. This made the code redundant because the logic for Sulfuras handled multiple conditions.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -45,12 +45,11 @@ class GildedRose {
                 }
             }
         }
-        else {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
+        else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
+        else if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 
