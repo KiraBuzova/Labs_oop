2d3d4be2f858b30bc5a9d05e8e1863625df4915d
Kira Buzova <kbuzova06@gmail.cpm>
2024-11-06 18:09:00 +0200

    Creating a new InventoryItem class. The new approach using the InventoryItem class makes the code more modular, maintainable and extensible. By encapsulating the logic of each element, GildedRose focuses only on managing the list of elements without dealing with the details. This improves the architecture and facilitates further development and testing, and allows for easy addition of new product types in the future.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,73 +9,8 @@ class GildedRose {
 
     public void updateInventory() {
         for (Item item : items) {
-            updateItem(item);
+            new InventoryItem(item).dailyUpdate();
         }
     }
 
-    private void updateItem(Item item) {
-        updateQuality(item);
-        updateExpiration(item);
-        if (isExpired(item)) {
-            processExpired(item);
-        }
-    }
-
-    private void updateQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-        }
-        else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            increaseQuality(item);
-
-            if (item.sellIn < 11) {
-                increaseQuality(item);
-            }
-
-            if (item.sellIn < 6) {
-                increaseQuality(item);
-            }
-        }
-        else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        else decreaseQuality(item);
-    }
-
-    private void updateExpiration(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        item.sellIn--;
-    }
-    
-    private boolean isExpired(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private void processExpired(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-        }
-        else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            item.quality = 0;
-        }
-        else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        } else {
-            decreaseQuality(item);
-        }
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
 }
---
+++ Java/src/main/java/com/gildedrose/InventoryItem.java
@@ -0,0 +1,75 @@
+package com.gildedrose;
+
+public class InventoryItem {
+    private final Item item;
+
+    public InventoryItem(Item item) {
+        this.item = item;
+    }
+
+    public void dailyUpdate() {
+        updateQuality();
+        updateExpiration();
+        if (isExpired()) {
+            processExpired();
+        }
+    }
+
+    protected void updateQuality() {
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality();
+        }
+        else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            increaseQuality();
+
+            if (item.sellIn < 11) {
+                increaseQuality();
+            }
+
+            if (item.sellIn < 6) {
+                increaseQuality();
+            }
+        }
+        else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
+        else decreaseQuality();
+    }
+
+    protected void updateExpiration() {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
+        item.sellIn--;
+    }
+
+    protected boolean isExpired() {
+        return item.sellIn < 0;
+    }
+
+    protected void processExpired() {
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality();
+        }
+        else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+        }
+        else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        } else {
+            decreaseQuality();
+        }
+    }
+
+    protected void increaseQuality() {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    protected void decreaseQuality() {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+}
